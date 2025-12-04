e03029c944c3fa55c88ee8711ea110f7d166cff1
Qi Qing <learn.qiqing@gmail.com>
2025-12-04 16:59:33 -0500

    turn on SpringApplication to check the Beans in main.

--- src/main/java/com/pluralsight/NorthwindTradersSpringBoot2/NorthwindTradersSpringBoot2Application.java
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot2/NorthwindTradersSpringBoot2Application.java
@@ -1,13 +1,17 @@
 package com.pluralsight.NorthwindTradersSpringBoot2;
 
+import com.pluralsight.NorthwindTradersSpringBoot2.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class NorthwindTradersSpringBoot2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersSpringBoot2Application.class, args);
+		ApplicationContext context = SpringApplication.run(NorthwindTradersSpringBoot2Application.class, args);
+
+		ProductService productService = context.getBean(ProductService.class);
 	}
 
 }
